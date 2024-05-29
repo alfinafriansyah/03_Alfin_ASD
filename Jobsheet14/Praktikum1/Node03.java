66aebf145193d04226dfa038a24943e5aa41ffad
afriansyah <alfinafriansyah68@gmail.com>
2024-05-29 21:58:45 +0700

    commit

---
+++ Jobsheet14/Praktikum1/Node03.java
@@ -0,0 +1,16 @@
+package Praktikum1;
+public class Node03 {
+    int data;
+    Node03 left;
+    Node03 right;
+
+    public Node03() {
+
+    }
+    public Node03(int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+    
+}
---
+++ Jobsheet14/Praktikum2/BinaryTreeArrayMain03.java
@@ -0,0 +1,19 @@
+package Praktikum2;
+
+public class BinaryTreeArrayMain03 {
+    public static void main(String[] args) {
+        BinaryTreeArray03 bta = new BinaryTreeArray03();
+        int[] data = {6,4,8,3,5,7,9,0,0,0};
+        int idxLast = 6;
+        bta.populateData(data, idxLast);
+        System.out.print("\nInOrder Traversal : ");
+        bta.traverseInOrder(0);
+        System.out.println();
+        System.out.println("PreOrder Traversal: ");
+        bta.traversePreOrder();
+        System.out.println();
+        System.out.println("PostOrder Traversal: ");
+        bta.traversePostOrder();
+
+    }
+}
